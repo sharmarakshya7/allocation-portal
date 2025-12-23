10a8af7e6e2e800cfceed0aba38c8cf73a32aed1
Rakshya <sharmarakshya7@gmail.com>
2025-12-23 12:45:17 -0600

    added frontend url for the CORS issue

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://allocation-portal.vercel.app",
+  "https://allocation-portal-frontend.vercel.app",
   process.env.FRONTEND_URL
 ];
 
